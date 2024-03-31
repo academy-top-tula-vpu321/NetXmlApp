a496bb1f9c0a3e24315c54720bb5f16d1caf1dad
maxefimov <efimov.top.academy@gmail.com>
2024-03-31 11:47:50 +0300

    Добавьте файлы проекта.

---
+++ NetXmlApp/Examples.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NetXmlApp
+{
+    static class Examples
+    {
+        static void XmlSerializeExample()
+        {
+            Address moscow = new Address() { City = "Moscow", Street = "Tverskaya" };
+            Address piter = new Address() { City = "St. Peterburg", Street = "Nevsky" };
+
+            Company yandex = new Company() { Title = "Yandex", Address = moscow };
+            Company piterSoft = new Company() { Title = "Piter Soft", Address = piter };
+            Company ozon = new Company() { Title = "Ozon", Address = moscow };
+
+            List<Employee> employees = new()
+            {
+                new()
+                {
+                    Name = "Bobby",
+                    BirthDate = new(2001, 4, 21),
+                    Company = yandex
+                },
+                new()
+                {
+                    Name = "Sammy",
+                    BirthDate = new(1999, 11, 4),
+                    Company = piterSoft
+                },
+                new()
+                {
+                    Name = "Jimmy",
+                    BirthDate = new(1989, 7, 16),
+                    Company = yandex
+                },
+                new()
+                {
+                    Name = "Tommy",
+                    BirthDate = new(2000, 6, 11),
+                    Company = piterSoft
+                },
+            };
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Employee>));
+
+            using (FileStream stream = new FileStream("employees.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(stream, employees);
+            }
+        }
+    }
+
+
+    public class Address
+    {
+        public string City { get; set; } = "";
+        public string Street { get; set; } = "";
+    }
+
+    public class Company
+    {
+        public string Title { set; get; } = "";
+        public Address? Address { get; set; }
+    }
+
+    public class Employee
+    {
+        public string Name { set; get; } = "";
+        public DateTime BirthDate { get; set; }
+        public Company? Company { get; set; }
+    }
+}
+
+
---
+++ NetXmlApp/Program.cs
@@ -0,0 +1,97 @@
+﻿using NetXmlApp;
+using System.Xml;
+using System.Xml.Serialization;
+
+Employee billy = new Employee()
+{
+    Name = "Billy",
+    BirthDate = new(1997, 8, 20),
+    Company = new()
+    {
+        Title = "Mail Group",
+        Address = new()
+        {
+            City = "Moscow",
+            Street = "Neglinskaya"
+        }
+    }
+};
+
+string xmlFileName = "employees.xml";
+
+XmlDocument xmlDocument = new XmlDocument();
+xmlDocument.Load(xmlFileName);
+
+XmlElement? root = xmlDocument.DocumentElement;
+
+if(root is not null)
+{
+
+    //foreach(XmlElement e in root)
+    //{
+    //    Console.WriteLine(e.Name);
+    //    foreach(XmlNode attr in e.Attributes)
+    //        Console.WriteLine($"\t{attr.Name} => {attr.Value}");
+    //    foreach (XmlNode n in e.ChildNodes)
+    //    {
+    //        Console.WriteLine($"\t{n.Name}");
+    //        foreach(XmlNode n2 in n.ChildNodes)
+    //            Console.WriteLine($"\t\t{n2.Name}");
+    //    }
+    //}
+
+    //ElementsPrint(root);
+
+
+    // Add Elements
+    //XmlElement xmlElement = xmlDocument.CreateElement(nameof(Employee));
+
+    //XmlAttribute xmlAttributePosition = xmlDocument.CreateAttribute("position");
+    //XmlText xmlTextAttributePosition = xmlDocument.CreateTextNode("developer");
+    //xmlAttributePosition.AppendChild(xmlTextAttributePosition);
+    //xmlElement.Attributes.Append(xmlAttributePosition);
+
+    //XmlElement childElement = xmlDocument.CreateElement(nameof(Employee.Name));
+
+    //childElement.InnerText = billy.Name;
+    //xmlElement.AppendChild(childElement);
+
+    //childElement = xmlDocument.CreateElement(nameof(Employee.BirthDate));
+    //childElement.InnerText = billy.BirthDate.ToString();
+    //xmlElement.AppendChild(childElement);
+
+    //childElement = xmlDocument.CreateElement(nameof(Employee.Company));
+    //childElement.AppendChild(xmlDocument.CreateElement("Title"));
+    //xmlElement.AppendChild(childElement);
+
+    //root.AppendChild(xmlElement);
+    //xmlDocument.Save(xmlFileName);
+
+    var nodeDel = root.LastChild;
+    if(nodeDel is not null)
+        root.RemoveChild(nodeDel);
+
+    xmlDocument.Save(xmlFileName);
+}
+
+void ElementsPrint(XmlElement element, int level = 0)
+{
+    string tabs = new String('\t', level);
+
+    Console.WriteLine();
+
+    Console.WriteLine($"{tabs}Element: {element.Name}");
+    Console.WriteLine($"{tabs}Value: {element.InnerText}");
+    //Console.WriteLine($"{tabs}Xml Value: {element.InnerXml}");
+
+    Console.WriteLine($"{tabs}Attributes:");
+    foreach (XmlAttribute attribute in element.Attributes)
+        Console.WriteLine($"{tabs}{attribute.Name}={attribute.Value}");
+
+    foreach (XmlNode node in element)
+    {
+        if (node.NodeType == XmlNodeType.Element)
+            ElementsPrint((node as XmlElement)!, level + 1);
+    }
+
+}
